37e7b6899845bbf003c48e5e6efec399afead455
Mohamed Infaz <infaz9999@gmail.com>
2018-04-01 12:15:28 +0530

    Added get todos test cases

--- tests/server.test.js
+++ tests/server.test.js
@@ -4,8 +4,12 @@ const {app}  = require('../server/server');
 const {Todo} = require('../models/todo');
 const {Users} = require('../models/user');
 
+var dummy = [{text : 'First todo get test'},{text : 'Second todo get test'}];
+
 beforeEach((done) => {
-    Todo.remove({}).then(() => done())
+    Todo.remove({}).then(() => {
+        return Todo.insertMany(dummy).then(() => done());
+    })
     .catch((e) => done(e));
 })
 
@@ -25,7 +29,7 @@ describe('POST/todos', () => {
                     return done(err);
                 }
 
-                Todo.find().then((todos) => {
+                Todo.find({text: text}).then((todos) => {
                     expect(todos.length).toBe(1);
                     expect(todos[0].text).toBe(text);
                     done();
@@ -33,10 +37,10 @@ describe('POST/todos', () => {
             });
     });
 
-    beforeEach((done) => {
-        Todo.remove({}).then(() => done())
-        .catch((e) => done(e));
-    })
+    // beforeEach((done) => {
+    //     Todo.remove({}).then(() => done())
+    //     .catch((e) => done(e));
+    // })
 
     it('Should not create todos when a bad request', (done) => {
 
@@ -50,9 +54,21 @@ describe('POST/todos', () => {
                 }
                 
                 Todo.find().then((todos) => {
-                    expect(todos.length).toBe(0);
+                    expect(todos.length).toBe(2);
                     done();
                 }).catch((e) => done(e));
             });
     });
+});
+
+describe('Get /todos', () => {
+    it('should get all todos', (done) => {
+        request(app)
+            .get('/todos')
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todos.length).toBe(2);
+            })
+            .end(done());
+    })
 });
