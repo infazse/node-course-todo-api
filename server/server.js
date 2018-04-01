d73f7a123617afb8b78d46a6386b65b1a328ae82
Mohamed Infaz <infaz9999@gmail.com>
2018-04-01 10:07:04 +0530

    added POST end points and refactored mongoose

---
+++ server/server.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var bodyparser = require('body-parser');
+
+var {mongoose} = require('../db/mongoose');
+var {user} = require('../models/user');
+var {Todo} = require('../models/todo');
+
+var app = express();
+app.use(bodyparser.json());
+app.post('/todos', (req, res) => {
+    console.log(req.body);
+    var todo = new Todo({
+        text : req.body.text
+    });
+
+    todo.save().then((result) => {
+        res.send(result);
+    }, (e) => {
+        res.status(400).send(e);
+    });
+});
+
+app.listen(3000, () => {
+    console.log('Started on port 3000')
+});
